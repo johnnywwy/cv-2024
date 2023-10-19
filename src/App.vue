b6a584c871335ce08cb4368b22a74e470917eb0e
wuwenyu <johnnywwy@gmail.com>
2023-10-19 08:52:27 +0800

    feat：2023版简历

---
+++ src/App.vue
@@ -0,0 +1,247 @@
+<template>
+  <article>
+    <!-- 个人信息 -->
+    <!-- <section class="bio">
+      <h1>吴文宇</h1>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/MiB.svg/440px-MiB.svg.png"
+        alt=""
+        style="width: 100px; height: 100px"
+      />
+      <p>男 | 27岁 | 前端开发工程师</p>
+      <p>
+        手机：<a href="tel:15622178416">15622178416</a> | 邮箱：<a
+          href="mailto:johnnywwy@163.com"
+          >johnnywwy@163.com</a
+        >| 微信：johnnywwy
+      </p>
+      <div style="height: 10px"></div>
+    </section> -->
+
+    <!-- 教育经历 -->
+    <!-- <section class="education">
+      <h2>教育经历</h2>
+      <p>五邑大学 电子信息专业 本科</p>
+      <div style="height: 10px"></div>
+    </section> -->
+
+    <!-- 工作经历 -->
+    <section class="work">
+      <h2>工作经历</h2>
+      <ol>
+        <!-- <li>
+          <header>
+            <h3>广东微壹科技有限公司（实习）</h3>
+            <span>2020 年 7 月~2020 年 9 月</span>
+          </header>
+          <p>
+            负责公司项目的缺陷修复以及开发工作，根据产品需求和 UI
+            设计师提供的高保图对静态页面的搭建以及样式的调整。
+          </p>
+        </li>
+        <li>
+          <header>
+            <h3>广东微壹科技有限公司（实习）</h3>
+            <span>2020 年 7 月~2020 年 9 月</span>
+          </header>
+          <p>
+            负责公司项目的缺陷修复以及开发工作，根据产品需求和 UI
+            设计师提供的高保图对静态页面的搭建以及样式的调整。
+          </p>
+        </li> -->
+        <li>
+          <!-- <header>
+            <h3>广州华夏汇海有限公司</h3>
+            <span>2021 年 6 月 ~ 至今</span>
+          </header> -->
+          <p>
+            使用 Vue3 + Element Plus + ts
+            框架对校园智慧体育后台管理系统进行功能的开发，维护，配合后端进行接口的联调完成页面的开发。
+          </p>
+          <p></p>
+          <!-- <p>配合开发人员使用 React-Native 从头搭建了一个移动端 App（菜品管理 App），负责使用封装好的组件实现第一阶段主体功能，
+          第二阶段主要负责了样式的调整（UI）与缺陷修复，同时新增了一些小功能。</p> -->
+          <p>完成多款小程序的上线，负责功能的迭加以及样式的调整。</p>
+        </li>
+      </ol>
+      <div style="height: 10px"></div>
+    </section>
+
+    <!-- 专业技能 -->
+    <!-- <section class="skills">
+      <h2>技能</h2>
+      <div class="wrapper">
+        <ul>
+          <li>熟悉页面制作技巧，能将设计稿<strong>1:1还原</strong></li>
+          <li>
+            熟悉<strong>前后端分离</strong>技术，包括
+            AJAX、跨域、前端路由、Cookie、Session 等
+          </li>
+          <li>
+            熟练掌握 <strong>Vue全家桶</strong>的使用，包括
+            VueCli、VueRouter、Vuex 等
+          </li>
+          <li>熟悉 ES6，包括 let / Promise / await / 析构赋值</li>
+          <li>
+            了解 <strong>TypeScript</strong> 的使用，我的项目就是 TS 实现的
+          </li>
+          <li>了解 <strong>Webpack 的配置和优化</strong></li>
+          <li>熟练使用 VS code、WebStorm 等开发工具</li>
+        </ul>
+      </div>
+      <div style="height: 10px"></div>
+    </section> -->
+
+    <!-- 项目经历 -->
+    <!-- <section class="projects">
+      <h2>项目经历</h2>
+      <p></p>
+      <ol>
+        <li>
+          <header>
+            <h3>后台管理系统</h3>
+            <span>
+              <a href="https://github.com">源码链接</a>
+            </span>
+          </header>
+          <p>
+            一款基于
+            <strong>Vue2 </strong>/ <strong>Vue-router </strong>/
+            <strong>Vuex </strong>/ <strong>Axios </strong>/
+            <strong>Element-ui </strong> 的后台管理系统
+          </p>
+          <p>
+            主要负责后台管理页面的管理<strong>用户账号</strong>、<strong>商品分类</strong>、<strong>商品信息</strong>、<strong>订单</strong>等业务功能
+          </p>
+          <p>
+            该项目采用前后端分离的开发模式，拥有<strong>登录管理</strong>，<strong>用户管理</strong>，<strong>权限管理</strong>，<strong>商品管理</strong>，<strong>订单管理</strong>等模块
+          </p>
+          <p>
+            通过这次项目让我对后台管理系统有个更深入的了解，掌握了后台管理系统的制作技巧
+          </p>
+          <p></p>
+          <p></p>
+        </li>
+        <li>
+          <header>
+            <h3>咕噜 UI</h3>
+            <span>
+              <a href="https://github.com">源码链接</a>
+              <a href="https://baidu.com">项目预览</a>
+            </span>
+          </header>
+          <p>
+            一款基于
+            <strong>Vue3 </strong>/ <strong>TypeScript </strong>/
+            <strong>Vite </strong> 的 UI 框架
+          </p>
+          <p>
+            目前UI组件有
+            <strong>Button组件</strong>、 <strong>Switch组件</strong>、
+            <strong>Dialog组件</strong>、
+            <strong>Tabs组件</strong>
+          </p>
+          <p>除了学习之外，主要目的是希望在工作中能够加以复用，提高开发效率</p>
+          <p>
+            制作过程让我对 Vue3 有了新的认识，并且分别对移动端和 PC
+            端进行适配，提升用户使用体验
+          </p>
+          <p>
+            并将整个项目的学习过程总结成了一篇博客《<a href="https://baidu.com"
+              >Vue3 造轮子实录</a
+            >》
+          </p>
+          <p></p>
+          <p></p>
+        </li>
+        <li>
+          <header>
+            <h3>云笔记</h3>
+            <span>
+              <a href="https://baidu.com">源码链接</a>
+              <a href="https://baidu.com">项目预览</a>
+            </span>
+          </header>
+          <p>
+            一款基于 <strong>Vue2</strong> / <strong>Vuex</strong> /
+            <strong>Element-ui</strong> 的 PC 版云笔记
+          </p>
+          <p>
+            主要功能：用户登录与注册，对笔记本进行增删改查等操作，对markdown格式进行渲染
+          </p>
+          <p>
+            制作过程让我对<strong>后端交互</strong>有了更深刻的理解，掌握了 Vuex
+            状态管理的使用，<strong>接口的封装</strong>，增强可复用性。
+            并将整个项目的学习过程总结成了一篇博客《<a href="https://baidu.com"
+              >云笔记开发实录</a
+            >》
+          </p>
+          <p></p>
+          <p></p>
+        </li>
+        <li>
+          <header>
+            <h3>大屏可视化</h3>
+            <span>
+              <a href="https://baidu.com">源码链接</a>
+              <a href="https://baidu.com">项目预览</a>
+            </span>
+          </header>
+          <p>
+            一款基于
+            <strong>Vue </strong>/
+            <strong>Echarts </strong>
+            的大屏可视化项目
+          </p>
+          <p>
+            为了适应所有屏幕，我使用了动态
+            <strong>rem</strong> 方案，具体思路见我的博客《<a
+              href="https://baidu.com"
+              >大屏项目实战</a
+            >》
+          </p>
+
+          <p>
+            这个项目是我在学习前端可视化与 echarts 时制作而成，页面布局使用了
+            <strong>Grid</strong> 和 <strong>Flex</strong> 布局
+          </p>
+
+          <p>
+            项目中包含的图例有柱形图、折线图、饼图、地图和表格等，图例都可以实时更新数据并辅以动画交互
+          </p>
+          <p>通过这个项目，我掌握了大屏可视化项目的制作技巧</p>
+        </li>
+        <li>
+          <header>
+            <h3>旺财记账</h3>
+            <span>
+              <a href="https://baidu.com">源码链接</a>
+              <a href="https://baidu.com">项目预览</a>
+            </span>
+          </header>
+          <p>
+            一个基于 <strong>Vue</strong> /
+            <strong>TypeScript</strong> 的移动端单页面应用
+          </p>
+          <p>
+            该项目使用 <strong>VueRouter</strong> 的 Hash
+            模式来实现不同页面的跳转
+          </p>
+          <p>
+            使用 <strong>Flex</strong> 布局来达到响应式布局，使用
+            <strong>Vuex</strong> 来进行全局数据管理
+          </p>
+          <p>这是我从自己的需求出发，设计出的极简记账应用，特点是快速记账</p>
+          <p></p>
+        </li>
+      </ol>
+      <div style="height: 10px"></div>
+    </section> -->
+  </article>
+</template>
+
+<script setup></script>
+
+<style scoped>
+@import "app.module.scss";
+</style>
