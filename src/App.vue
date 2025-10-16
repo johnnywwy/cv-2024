6dcd7a261f113c2542c7bbf5f0239bf22702043c
johnnywwy <johnnywwy@gmail.com>
2025-10-16 21:44:30 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <section class="bio">
       <h1>吴文宇</h1>
       <img src="./assets/wwy.jpg" alt="" style="width: 100px; height: 100px" />
-      <p>男 | 27岁 | 前端开发工程师</p>
+      <p>男 | 28岁 | 全栈开发工程师</p>
       <p>
         手机：<a href="tel:15622178416">15622178416</a> | 邮箱：<a href="mailto:johnnywwy@163.com">johnnywwy@163.com</a>|
         微信：johnnywwy
@@ -13,11 +13,11 @@
     </section>
 
     <!-- 教育经历 -->
-    <!-- <section class="education">
-      <h2>教育经历</h2>
+    <section class="education">
+      <h2 class="title underline">教育经历</h2>
       <p>五邑大学 电子信息专业 本科</p>
       <div style="height: 10px"></div>
-    </section> -->
+    </section>
 
     <!-- 工作经历 -->
     <section class="work">
@@ -25,75 +25,72 @@
       <ol>
         <li>
           <header style="margin-bottom: 12px">
-            <h3 style="font-size: 22px;">广州华夏汇海科技有限公司</h3>
-            <span>2020 年 12 月 ~ 至今</span>
+            <h3 style="font-size: 22px;">珠海爱蒲京软件</h3>
+            <span>2024 年 10 月 ~ 至今</span>
           </header>
           <main>
-            <!-- 项目1 -->
-            <h3 style="margin-bottom: 8px;font-size: 18px;">校园智慧跳绳平台</h3>
-            <div style="margin-bottom: 8px">
-              <span class="tag">React</span>
-              <span class="tag">Ant Design</span>
-              <span class="tag">React-router-dom</span>
-              <span class="tag">Axios</span>
-              <span class="tag">Redux</span>
-              <span class="tag">Vite</span>
 
-              <span class="tag">Websocket</span>
+            <!-- 项目1 -->
+            <h3 style="margin-bottom: 8px; font-size: 18px;">SHSOP</h3>
+            <div style="margin-bottom: 8px">
+              <span class="tag">Nest.js</span>
+              <span class="tag">Nodejs</span>
+              <span class="tag">MongoDB</span>
+              <span class="tag">Redis</span>
             </div>
             <div class="content">
               <p>
                 <b>项目描述</b>：
-                与公司智能跳绳相结合，面向中小学师生，针对校园跳绳场景，集跳绳管理、作业、比赛、课堂、产品管理、物联网卡跟踪等，共计15个模块，帮助学生提升跳绳技能、监测体育锻炼状况，并为教师提供便捷的管理和数据分析工具
+                SHSOP 一站式策略性健康服务运作平台，一个大型的医疗平台，集成各种香港政府相关医疗政策实施的入口，并为相关人员提供各种医疗服务
               </p>
               <p>
                 <b>工作内容</b>
               <div style="margin-left: 1em">
                 <p>
-                  1. 从零开始进行项目调研，搭建了基于 React
-                  的前端架构，确定了整体的技术栈，包括 <strong>React hooks、Redux、React Router</strong>
-                  等核心技术，确保了项目的技术选型与业务需求的紧密结合。
+                  1. 负责多个政府资助或合作项目业务，如 CDCC（慢性疾病共同治理计划）、Dental PPP（牙科公私营合作计划）、MvCarer（移动照护服务） 等等
                 </p>
                 <p>
-                  2. 实现动态路由和按钮级别权限控制功能，React 的<strong>组件化</strong>思想和 Redux
-                  状态管理库
+                  2. 系统提供多种核心业务模块，包括：
+                  人员登记（Enrol）、医生档案（DrProfile）、临床管理（Clinical / ClinicalTeam）、转介管理（Referral）、参与者管理（Participant）
+                  等，支撑不同医疗项目的统一运营。
                 </p>
-                <p>
-                  3.
-                  针对项目进行优化，提升平台性能与用户满意度，并顺利完成项目的上线工作
-                </p>
+
               </div>
               </p>
               <p>
-                <b>项目难点</b>
+                <b>项目亮点</b>
               <div style="margin-left: 1em">
                 <p>
-                  1. <b>WebSocket
-                    实时通信</b>：与智能跳绳设备实时连接，确保数据同步和展示。具备<strong>自动重连</strong>、<strong>心跳检测</strong>功能，提升连接稳定性和数据传输可靠性
+                  1. 项目采用 <strong>NestJS</strong> 构建后端服务，基于 微服务架构（Microservices Architecture） 设计，实现各业务模块（如
+                  enrol、drprofile、referral、participant 等）的独立部署与灵活扩展，提升系统的可维护性与稳定性。
                 </p>
+
                 <p>
-                  2. <b>解析
-                    Excel</b>：根据业务需求，封装了通用 Excel 文件转 JSON
-                  的导入组件，并发布到公司 npm
-                  私有源供团队各个项目中使用。该组件支持高度自定义和数据处理功能，包括可<strong>根据字典配置自动映射相关数据</strong>，<strong>支持分页上传</strong>、<strong>自动表头匹配</strong>、<strong>数据校验与错误提示</strong>、显著提升了数据导入的效率和准确性。
+                  2. <b>前后端分离设计</b>：后端通过 RESTful API 提供统一接口服务，前端独立开发与部署，提升了团队并行开发效率与模块解耦度，支持多项目统一集成。
                 </p>
+
                 <p>
-                  3. <b>数据可视化分析</b>： 利用 ECharts 提供多种图表和可视化组件，将跳绳数据和运动表现通过直观的图表展示，通过时间轴功能展示历史数据，方便用户追踪和回顾长期运动表现及成绩变化。
+                  3. 系统运行于 <b>OpenShift 容器平台</b>, 结合 CI/CD 流程实现自动化构建、部署与回滚，确保不同环境（DEV/UAT/PROD）的一致性与快速交付。
                 </p>
+
                 <p>
-                  4.
-                  <b>权限管理</b>：实现<strong>权限管理</strong>，包括对<strong>用户管理</strong>、<strong>角色管理</strong>与<strong>菜单管理</strong>，使用户权限精确到按钮级别
+                  4. 团队采用 <strong>Scrum 敏捷开发流程</strong>，按迭代（Sprint）进行需求规划、开发与测试，灵活响应业务变更，持续优化系统性能与用户体验。
                 </p>
               </div>
               </p>
-
-
+              <br />
             </div>
 
+          </main>
+        </li>
+        <li>
+          <header style="margin-bottom: 12px">
+            <h3 style="font-size: 22px;">广州华夏汇海科技有限公司</h3>
+            <span>2022 年 11 月 ~ 2024 年 9 月</span>
+          </header>
+          <main>
 
-            <br />
-
-            <!-- 项目2 -->
+            <!-- 项目1 -->
             <h3 style="margin-bottom: 8px; font-size: 18px;">智慧体育云平台</h3>
             <div style="margin-bottom: 8px">
               <span class="tag">React</span>
@@ -123,22 +120,89 @@
                 <b>项目亮点</b>
               <div style="margin-left: 1em">
                 <p>
-                  1. <b>前端工程化</b>：通过 <strong>Vite</strong> 构建工具提升项目的开发和构建效率，优化了热更新速度并减少了打包体积。同时，使用
+                  1. <b>Node.js采集中转服务</b>：由于架构迁移，浏览器安全策略问题，导致新架构无法对接公司旧设备 WIFI 采集相关功能，使用<strong> Node.js </strong>
+                  编写服务，并将其打包成 exe，对外暴露端口，供前端显示搜索并获取采集设备相关数据。
+                </p>
+                <p>
+                  2. <b>前端工程化</b>：通过 <strong>Vite</strong> 构建工具提升项目的开发和构建效率，优化了热更新速度并减少了打包体积。同时，使用
                   <strong>ESLint</strong>、<strong>Prettier</strong> 和
                   <strong>Husky</strong>
-                  实现代码规范化管理，确保团队协作中的代码质量和一致性
+                  实现代码规范化管理，确保团队协作中的代码质量和一致性。
                 </p>
+
                 <p>
-                  2. <b>Node.js采集中转服务</b>：由于架构迁移，浏览器安全策略问题，导致新架构无法对接公司旧设备 WIFI 采集相关功能，使用<strong>Node.js</strong>
-                  编写服务，并对外暴露端口，搜索并获取采集设备相关数据，并实时推送至前端
-                </p>
-                <p>
-                  3. <b>文件上传组件</b>： 支持图片、文件上传，并集成了文件预览和多图上传功能，并上传至<strong>阿里云OSS</strong>，解决了文件上传与图片管理的复杂场景
+                  3. <b>文件上传组件</b>： 支持图片、文件上传，并集成了文件预览和多图上传功能，并上传至<strong>阿里云OSS</strong>，解决了文件上传与图片管理的复杂场景。
                 </p>
               </div>
               </p>
               <br />
             </div>
+
+            <!-- 项目2 -->
+            <h3 style="margin-bottom: 8px;font-size: 18px;">校园智慧跳绳平台</h3>
+            <div style="margin-bottom: 8px">
+              <span class="tag">React</span>
+              <span class="tag">Ant Design</span>
+              <span class="tag">React-router-dom</span>
+              <span class="tag">Axios</span>
+              <span class="tag">Redux</span>
+              <span class="tag">Vite</span>
+
+              <span class="tag">Websocket</span>
+            </div>
+            <div class="content">
+              <p>
+                <b>项目描述</b>：
+                与公司智能跳绳相结合，面向中小学师生，针对校园跳绳场景，集跳绳管理、作业、比赛、课堂、产品管理、物联网卡跟踪等，共计15个模块，帮助学生提升跳绳技能、监测体育锻炼状况，并为教师提供便捷的管理和数据分析工具。
+              </p>
+              <p>
+                <b>工作内容</b>
+              <div style="margin-left: 1em">
+                <p>
+                  1. 从零开始进行项目调研，搭建了基于 React
+                  的前端架构，确定了整体的技术栈，包括 <strong>React hooks、Redux、React Router</strong>
+                  等核心技术，确保了项目的技术选型与业务需求的紧密结合。
+                </p>
+                <p>
+                  2. 实现动态路由和按钮级别权限控制功能，React 的<strong>组件化</strong>思想和 Redux
+                  状态管理库。
+                </p>
+                <p>
+                  3.
+                  针对项目进行优化，提升平台性能与用户满意度，并顺利完成项目的上线工作。
+                </p>
+              </div>
+              </p>
+              <p>
+                <b>项目难点</b>
+              <div style="margin-left: 1em">
+                <p>
+                  1. <b>WebSocket
+                    实时通信</b>：与智能跳绳设备实时连接，确保数据同步和展示。具备<strong>自动重连</strong>、<strong>心跳检测</strong>功能，提升连接稳定性和数据传输可靠性
+                </p>
+                <p>
+                  2. <b>解析
+                    Excel</b>：根据业务需求，封装了通用 Excel 文件转 JSON
+                  的导入组件，并发布到公司 npm
+                  私有源供团队各个项目中使用。该组件支持高度自定义和数据处理功能，包括可<strong>根据字典配置自动映射相关数据</strong>，<strong>支持分页上传</strong>、<strong>自动表头匹配</strong>、<strong>数据校验与错误提示</strong>、显著提升了数据导入的效率和准确性。
+                </p>
+                <p>
+                  3. <b>数据可视化分析</b>： 利用 ECharts 提供多种图表和可视化组件，将跳绳数据和运动表现通过直观的图表展示，通过时间轴功能展示历史数据，方便用户追踪和回顾长期运动表现及成绩变化。
+                </p>
+                <p>
+                  4.
+                  <b>权限管理</b>：实现<strong>权限管理</strong>，包括对<strong>用户管理</strong>、<strong>角色管理</strong>与<strong>菜单管理</strong>，使用户权限精确到按钮级别。
+                </p>
+              </div>
+              </p>
+
+
+            </div>
+
+
+            <br />
+
+
 
             <!-- 项目3 -->
             <h3 style="margin-bottom: 8px ;font-size: 18px;">校园智慧跳绳小程序</h3>
@@ -160,10 +224,10 @@
                 <b>工作内容</b>
               <div style="margin-left: 1em">
                 <p>
-                  1. 独立实现小程序<strong>从0到1</strong>的申请、审批、开发、测试、上线以及后续维护全流程
+                  1. 独立实现小程序<strong>从0到1</strong>的申请、审批、开发、测试、上线以及后续维护全流程。
                 </p>
                 <p>
-                  2. 根据UI提供的设计规范，完成负责前端项目日常需求开发
+                  2. 根据UI提供的设计规范，完成负责前端项目日常需求开发。
                 </p>
               </div>
               </p>
@@ -171,14 +235,14 @@
                 <b>项目亮点</b>
               <div style="margin-left: 1em">
                 <p>
-                  1. 独立实现使用 <strong>vite 搭建</strong> uniapp 开发平台，引入 <strong>Unocss 原子化</strong> css，并在公司内推广使用
+                  1. 独立实现使用 <strong>vite 搭建</strong> uniapp 开发平台，引入 <strong>Unocss 原子化</strong> css，并在公司内推广使用。
                 </p>
                 <p>
-                  2. 封装通用 Axios 请求，支持<strong>全局异常拦截</strong>、错误处理及请求头配置，提升了 API 请求管理的统一性和可维护性
+                  2. 封装通用 Axios 请求，支持<strong>全局异常拦截</strong>、错误处理及请求头配置，提升了 API 请求管理的统一性和可维护性。
                 </p>
                 <p>
                   3. 通过 <strong>WebSocket</strong>
-                  私有协议实现在小程序上<strong>连接</strong>并<strong>控制</strong>智能跳绳设备，并实时获取跳绳数据
+                  私有协议实现在小程序上<strong>连接</strong>并<strong>控制</strong>智能跳绳设备，并实时获取跳绳数据。
                 </p>
               </div>
               </p>
@@ -203,33 +267,37 @@
             </span>
           </header>
           <div style="margin-bottom: 8px">
+            <span class="tag">NestJS</span>
+            <span class="tag">TypeORM</span>
+            <span class="tag">Mysql</span>
+            <span class="tag">Redis</span>
             <span class="tag">React</span>
             <span class="tag">Ant Design</span>
             <span class="tag">pnpm</span>
             <span class="tag">Vite</span>
-            <span class="tag">NestJS</span>
-            <span class="tag">Mysql</span>
-            <span class="tag">Redis</span>
           </div>
           <div class="content">
             <p>
               <b>项目描述</b>：
-              一款基于 web 端的会议室预约系统，含有管理端和客户端。使用 NestJS + Mysql + Redis 实现后端业务，使用 React 实现前端场景。
+              一款基于 web 端的会议室预约系统，含有管理端和用户端。使用 NestJS + Mysql + Redis 实现后端业务，使用 React 实现前端场景。
             </p>
             <p>
               <b>工作内容</b>
             <div style="margin-left: 1em">
               <p>
                 1.
-                <strong>从0到1实现</strong>，在私有Linux设备上部署<strong>Docker</strong>，<strong>Mysql</strong>，<strong>Redis</strong>等所需技术。了解
-                DockerFile 编写流程
+                <strong>使用 Nest-cli
+                  从0到1搭建项目</strong>，在私有Linux设备上部署<strong>Docker</strong>，<strong>Mysql</strong>，<strong>Redis</strong>等所需技术。
               </p>
               <p>
-                2. 实现基于 <strong>RBAC</strong> 的角色权限控制，使用 <strong>JWT</strong> 作为系统验证登录方式，配合个人 163 邮箱实现登录验证
+                2. 实现基于<strong>RBAC</strong>的角色权限控制，使用<strong>JWT</strong>作为系统验证登录方式，配合个人 163 邮箱实现登录验证。
               </p>
               <p>
-                3. 使用 <strong>RESTful Api</strong> 进行接口规范化开发，并引入ApiFox 等工具完成接口初步调试
+                3. 使用 <strong>RESTful Api</strong> 进行接口规范化开发，并引入 ApiFox 等工具完成接口初步调试。
               </p>
+              <!-- <p>
+                4. 使用 <strong>RESTful Api</strong> 进行接口规范化开发，并引入 ApiFox 等工具完成接口初步调试。
+              </p> -->
             </div>
             </p>
           </div>
@@ -340,7 +408,7 @@
           <p></p>
         </li> -->
       </ol>
-      <div style="height: 10px"></div>
+      <!-- <div style="height: 10px"></div> -->
     </section>
 
 
