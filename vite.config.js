6ebe06dc0017992374648a323a116f01c2609441
johnnywwy <johnnywwy@gmail.com>
2026-09-25 16:36:47 +0800

    feat: 2.1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/cv-wwy/',
+  base: '/cv-2024/',
   build: {
     rollupOptions: {
       output: {
